6372c6e81c33eadcf80b1aa35eb26b7c677572cb
Hizkiel-Mesfin <mesfinezekiel@gmail.com>
2022-08-29 18:28:31 +0200

    done

--- index.js
+++ index.js
@@ -2,5 +2,26 @@ const toDosStore = ["Shopping", "Home work", "Go to the gym"];
 
 // Write your code here
 
+//Add item
 
-console.log(RenderToDosListTemplate())
+const insertTodo = (...item) => toDosStore.push(...item);
+
+insertTodo("Programming", "shower", "healthy meal", ["Shopping", "Test"]);
+console.log(toDosStore);
+
+//Remove an item
+
+const removeTodo = (num) => toDosStore.splice(num, 1);
+
+removeTodo(6);
+
+console.log(toDosStore);
+
+//Edit an item
+
+const editTodo = (index, replace, newItem) =>
+  toDosStore.splice(index, 1, newItem);
+
+editTodo(0, 1, "Fucking");
+
+console.log(toDosStore);
